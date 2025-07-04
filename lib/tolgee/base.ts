9b9d7db34c09ff0c77192f8a07da863ae8367e61
Gino Naumann <g@gnaumann.de>
2025-07-04 10:50:08 +0200

    Add apiKey on dev

--- lib/tolgee/base.ts
+++ lib/tolgee/base.ts
@@ -6,17 +6,21 @@ export enum LOCALES {
   en = 'en'
 }
 
-const apiKey = process.env.NEXT_PUBLIC_TOLGEE_API_KEY
-const apiUrl = process.env.NEXT_PUBLIC_TOLGEE_API_URL
-
 export function TolgeeBase() {
-  return Tolgee()
+  const tolgee = Tolgee()
     .use(FormatSimple()) // Super light formatter, which will enable you to pass variables into translations.
     .use(DevTools()) // automatically omitted in production builds - useful in development
     .use(CreateFunctionBackend({loader: fetchTolgee})) // custom backend loader
-    .updateDefaults({
+
+  if (process.env.NODE_ENV === 'development') {
+    const apiKey = process.env.NEXT_PUBLIC_TOLGEE_API_KEY
+    const apiUrl = process.env.NEXT_PUBLIC_TOLGEE_API_URL
+    tolgee.updateDefaults({
       // add api key and api url
       apiKey,
       apiUrl
     })
+  }
+
+  return tolgee
 }
